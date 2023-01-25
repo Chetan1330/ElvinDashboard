f084f8d9e3b4761aea4e408b627fd6a27ce9e3e5
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2023-01-25 14:46:37 +0530

    initial

---
+++ HrmDashboard/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('report/incomeexpense/', views.incomeexpense, name='incomeexpense')
+]
+
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
